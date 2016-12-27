321a77ada28ab66774e74df4b8b9541c1eea491d
shagun Sodhani <sshagunsodhani@gmail.com>
2016-12-27 22:13:13 +0530

    Added commit class

--- github/event.py
+++ github/event.py
@@ -3,6 +3,25 @@ import iso8601
 from util.constant import *
 
 
+class Commit:
+    '''Class to handle the commit object'''
+
+    def __init__(self, commit_dict):
+        self.commit_dict = commit_dict
+
+    def is_distinct(self):
+        return self.commit_dict['distinct']
+
+    def get_message(self):
+        return self.commit_dict['message']
+
+    def get_url(self):
+        return self.commit_dict['url']\
+            .replace('api.', '')\
+            .replace('repos/', '')\
+            .replace('commits', 'commit')
+
+
 class Event:
     '''Class to handle events returned by github api.'''
 
@@ -23,3 +42,12 @@ class Event:
     def get_repo_name(self):
         '''Method to return the repo name'''
         return self.event_dict[REPO][NAME]
+
+    def get_distinct_commits(self):
+        '''Method to return a list of commits belonging to the event.'''
+        if 'commits' in self.event_dict['payload']:
+            return (filter
+                    (lambda commit: commit.is_distinct(),
+                     map(lambda commit: Commit(commit),
+                         self.event_dict['payload']['commits'])))
+            return
