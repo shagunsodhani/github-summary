9039df13e8dc4dfbe298660657ed8adb28af9514
shagun Sodhani <sshagunsodhani@gmail.com>
2016-12-27 21:29:50 +0530

    added placeholder method to generate summary of commits and using config dict to pass around params

--- github/activity.py
+++ github/activity.py
@@ -13,17 +13,17 @@ headers = {
 }
 
 
-def get_activity_user(username=config_dict[USERNAME],
-                      since_date=datetime.now().timestamp() - 604800,
-                      client_id=config_dict[CLIENT_ID],
-                      client_secret=config_dict[CLIENT_SECRET]):
+def get_activity_user():
     '''Method to get all the activity of an user on github since `since_date`.
     Github API returns 30 items for each request (page).
     Fetching up to ten pages is supported, for a total of 300 events.
     Only events created within the past 90 days can be retrived.
     From: https://developer.github.com/v3/activity/events/#list-public-events
     '''
-
+    username = config_dict[USERNAME],
+    since_date = config_dict[SINCEDATE],
+    client_id = config_dict[CLIENT_ID],
+    client_secret = config_dict[CLIENT_SECRET]
     endpoint = "{base_url}/users/{username}/events?client_id={client_id}" \
                "&client_secret={client_secret}".format(base_url=base_url,
                                                        username=username,
@@ -33,7 +33,7 @@ def get_activity_user(username=config_dict[USERNAME],
     response = r.get(url=endpoint, headers=headers).json()
     while (response):
         for _event in response:
-            event = Event(event)
+            event = Event(_event)
             try:
                 created_at = event.created_at()
                 if (created_at < since_date):
@@ -50,22 +50,20 @@ def get_activity_user(username=config_dict[USERNAME],
             return
 
 
-def get_commits_user(username=config_dict[USERNAME],
-                     since_date=datetime.now().timestamp() - 604800,
-                     client_id=config_dict[CLIENT_ID],
-                     client_secret=config_dict[CLIENT_SECRET]):
+def get_commits_user(config_dict):
     '''Method to get all the commits of an user on github since `since_date`.
     Refer get_activity_user method for limits in number of commits that can be retrived.'''
     return filter(
-        lambda event: event.check_event_type(PUSHEVENT), get_activity_user(username,
-                                                                           since_date,
-                                                                           client_id,
-                                                                           client_secret)
+        lambda event: event.check_event_type(PUSHEVENT), get_activity_user(config_dict)
     )
 
 
-for i in get_commits_user(username=config_dict[USERNAME],
-                          since_date=datetime.now().timestamp() - 604800,
-                          client_id=config_dict[CLIENT_ID],
-                          client_secret=config_dict[CLIENT_SECRET]):
-    print(i['repo']['name'])
+def summarise_commits(config_dict):
+    '''Method to summarise the commits since `since_date`.'''
+
+    for event in get_commits_user(config_dict):
+        print(event.get_repo_name())
+
+
+config_dict[SINCEDATE] = datetime.now().timestamp() - 604800
+summarise_commits(config_dict)
