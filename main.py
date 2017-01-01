ad46b38a011239309ef6bdcfb9a3208a6a84f576
shagun Sodhani <sshagunsodhani@gmail.com>
2017-01-01 17:04:42 +0530

    Added a parameter in the config to control how much older commits are retrived

--- main.py
+++ main.py
@@ -6,5 +6,9 @@ from util.constant import *
 
 config_dict = parse_config(app_name=GITHUB)
 
-config_dict[SINCEDATE] = datetime.now().timestamp() - 604800
+if (config_dict[LOOKBACK_TIME]):
+    config_dict[SINCEDATE] = datetime.now().timestamp() - int(config_dict[LOOKBACK_TIME]    )
+else:
+    config_dict[SINCEDATE] = datetime.now().timestamp() - 604800
+
 summarise_commits(config_dict)
