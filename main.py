7e052c5d84f6c5ab55e2813dfabeb209c99e3e2e
shagun Sodhani <sshagunsodhani@gmail.com>
2017-01-06 21:25:55 +0530

    Added more formats to specify time. Related to #4

--- main.py
+++ main.py
@@ -6,9 +6,20 @@ from util.constant import *
 
 config_dict = parse_config(app_name=GITHUB)
 
+lookback_time = 604800
+
 if (config_dict[LOOKBACK_TIME]):
-    config_dict[SINCEDATE] = datetime.now().timestamp() - int(config_dict[LOOKBACK_TIME])
-else:
-    config_dict[SINCEDATE] = datetime.now().timestamp() - 604800
+    time_unit = config_dict[LOOKBACK_TIME][-1]
+    time_unit_map = {
+        's': 1,
+        'm': 60,
+        'h': 60 * 60,
+        'd': 60 * 60 * 24,
+        'w': 60 * 60 * 24 * 7
+    }
+    if(time_unit in time_unit_map):
+        lookback_time = int(config_dict[LOOKBACK_TIME][:-1]) * time_unit_map[time_unit]
+
+config_dict[SINCEDATE] = datetime.now().timestamp() - lookback_time
 
 summarise_commits(config_dict)
